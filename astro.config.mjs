9015519c0a248b04a59eaa77db0f4f667c4e8eb2
csabahi <sabahi.camron@gmail.com>
2026-09-20 17:21:45 -0400

    update to new name

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://uwgoats.github.io',
+  site: 'https://uwhom-e.github.io',
 
   vite: {
     plugins: [tailwindcss()]
